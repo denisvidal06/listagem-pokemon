2c928cfcdc7009a1a8895168d0f3e3006432ca99
DenisVidal06 <crfdenis0606@hotmail.com>
2024-11-13 22:59:55 -0400

    adicionado arquivo js

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaotrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click, () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro");
+
+    if (modoEscuroEstaAtivo) {
+        imagemBotaotrocaDeTema.setAttribute("src", "./src/imagem/sun.png");
+    }   else {
+        imagemBotaotrocaDeTema.setAttribute("src", "./src/imagem/moon.png");
+    }
+});
