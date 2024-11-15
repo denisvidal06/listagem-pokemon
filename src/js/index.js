a3defcbd8075e2db394c81b699648ca75db3b5ec
DenisVidal06 <crfdenis0606@hotmail.com>
2024-11-15 19:58:44 -0400

    alterado no html e no js

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,14 @@ const botaoAlterarTema = document.getElementById("botao-alterar-tema");
 const body = document.querySelector("body");
 const imagemBotaotrocaDeTema = document.querySelector(".imagem-botao");
 
-botaoAlterarTema.addEventListener("click, () => {
+botaoAlterarTema.addEventListener("click", () => {
     const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
 
     body.classList.toggle("modo-escuro");
 
     if (modoEscuroEstaAtivo) {
         imagemBotaotrocaDeTema.setAttribute("src", "./src/imagem/sun.png");
-    }   else {
+    } else {
         imagemBotaotrocaDeTema.setAttribute("src", "./src/imagem/moon.png");
     }
 });
